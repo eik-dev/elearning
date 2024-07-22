df5ac6218e68c5024c007a7b19050e7ba3126d4a
TheParadox20 <onyambusamson@gmail.com>
2024-07-23 01:20:22 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <video src="https://drive.google.com/file/d/1mR_5LVomn9UnzdT6uh-QkyDGx6ex73mr/view?usp=sharing"></video>
+    </main>
+  );
+}
